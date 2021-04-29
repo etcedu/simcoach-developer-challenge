a9b2f719bb0da598e82d0cdfef4aa102b977b288
Garrett Kimball <gkimball@simcoachgames.com>
2021-04-29 10:55:44 -0400

    Ready For Use

---
+++ Assets/Scripts/DozerControl.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DozerControl : MonoBehaviour
+{
+    public int driveSpeed;
+    public int turnSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.forward * driveSpeed * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.forward* driveSpeed * Time.deltaTime * -1;
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(new Vector3(0, -1, 0) * turnSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(new Vector3(0, 1, 0) * turnSpeed * Time.deltaTime);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.tag == "pushable")
+        {
+            FindObjectOfType<DozerGameManager>().HitDirt();
+            Destroy(collision.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/DozerGameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DozerGameManager : MonoBehaviour
+{
+    public int currentStage;
+    public Text currentStageLabel;
+
+    public GameObject dirtPilePrefab;
+    
+    int dirtRemaining;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentStage = 1;
+        SpawnDirt(currentStage);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentStageLabel.text = "Stage: " + currentStage.ToString();
+    }
+
+    void SpawnDirt(int numDirt)
+    {
+        dirtRemaining = 0;
+        for (int i = 0; i < numDirt; i++)
+        {
+            GameObject newDirt = Instantiate(dirtPilePrefab);
+            newDirt.transform.position = new Vector3(Random.Range(-28f, 28f), 1, Random.Range(-20f, 20f));
+            dirtRemaining++;
+        }
+    }
+
+    public void HitDirt()
+    {
+        dirtRemaining--;
+        if (dirtRemaining <= 0)
+        {
+            currentStage++;
+            SpawnDirt(currentStage);
+        }
+    }
+}
